f7e4dd80ac13cce2648541dc933f5f838e09f0ba
Mousa Khan <mousakhan95@gmail.com>
2017-06-12 15:57:43 +0200

    A bit of clean up, dealing with textfield UX

---
+++ ViiMe/TextFieldHelper.swift
@@ -0,0 +1,9 @@
+//
+//  TextFieldHelper.swift
+//  ViiMe
+//
+//  Created by Mousa Khan on 17-06-12.
+//  Copyright © 2017 Venture Lifestyles. All rights reserved.
+//
+
+import Foundation
---
+++ ViiMe/ValidationHelper.swift
@@ -0,0 +1,9 @@
+//
+//  ValidationHelper.swift
+//  ViiMe
+//
+//  Created by Mousa Khan on 17-06-12.
+//  Copyright © 2017 Venture Lifestyles. All rights reserved.
+//
+
+import Foundation
