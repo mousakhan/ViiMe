00e7b4f1f11159d2bbbed182806eb34f7dbb1a99
Mousa Khan <mousakhan95@gmail.com>
2017-07-29 16:02:51 +0200

    Moving some files where they should be

--- ViiMe/UIImageView+Download.swift
+++ ViiMe/UIImageView+Download.swift
@@ -2,7 +2,7 @@
 //  UIImageView+Download.swift
 //  ViiMe
 //
-//  Created by Mousa Khan on 17-07-26.
+//  Created by Mousa Khan on 17-07-28.
 //  Copyright © 2017 Venture Lifestyles. All rights reserved.
 //
 
@@ -12,7 +12,8 @@ extension UIImageView {
     func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
         contentMode = mode
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
                 let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
                 let data = data, error == nil,
                 let image = UIImage(data: data)
@@ -22,7 +23,7 @@ extension UIImageView {
             }
             }.resume()
     }
-    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFill) {
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
         guard let url = URL(string: link) else { return }
         downloadedFrom(url: url, contentMode: mode)
     }
